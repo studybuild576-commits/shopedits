5c8328330703563011e6d173fa5438536d24dd62
studybuild576-commits <studybuild576@gmail.com>
2025-10-07 07:03:25 +0530

    Create id_card_maker_page.dart

---
+++ lib/pages/id_card_maker_page.dart
@@ -0,0 +1,16 @@
+// Is page ka code abhi ke liye placeholder hai, ise baad mein banayenge.
+import 'package:flutter/material.dart';
+
+class IDCardMakerPage extends StatelessWidget {
+  const IDCardMakerPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('ID Card Maker')),
+      body: const Center(
+        child: Text('ID Card Maker Feature Coming Soon!'),
+      ),
+    );
+  }
+}
